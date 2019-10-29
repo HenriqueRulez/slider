cf488670da9f53c37c82df4eff0bd4d2cab2f83f
Henrique <mirandaferreirahenrique@hotmail.com>
2019-10-29 10:51:15 -0300

    custom control style

--- js/slide.js
+++ js/slide.js
@@ -207,7 +207,6 @@ export class SlideNav extends Slide {
     this.control =
       document.querySelector(customControl) || this.createControl();
     this.controlArray = [...this.control.children];
-    console.log(this.controlArray);
     this.activeControlItem();
     this.controlArray.forEach(this.eventControl);
   }
